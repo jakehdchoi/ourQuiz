6b976c536b162ebac40fa88ce27eb0574e52811d
jakehdchoi <jakehdchoi@gmail.com>
2016-08-20 19:51:57 +0900

    리스트를 표현하였다
    아직은 DB가 없으니 그냥 변수로 입력하였다.
    tmpl_userList.js에서 입력해줬다.

--- client/tmpl_userList.js
+++ client/tmpl_userList.js
@@ -1,6 +1,15 @@
 Template.userList.helpers({
 	listName : function(){
 		return "사용자목록";
+	},
+	list : function(){
+		var arr = [
+			{no:4, name:"aaa" ,email:"aaa@email.com"},
+			{no:1, name:"ddd", email:"ddd@email.com"},
+			{no:3, name:"bbb" ,email:"bbb@email.com"},
+			{no:2, name:"ccc" ,email:"ccc@email.com"}
+		];
+		return arr;
 	}
 });
 
